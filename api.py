ed5eb1ba4b5ce6a483c3d112abe570e254003523
mpcarolin <mpcarolin@gmail.com>
2017-11-11 17:10:58 -0700

    Implemented the basic calling structure between JsonMapper and BaseApi

--- api.py
+++ api.py
@@ -1,14 +1,42 @@
 from flask_api import FlaskAPI
+import json
 
-app = FlaskAPI(__name__)
+with open('config.json', 'r') as config:
+    json = json.loads(config.read())
+    PROJECT_NAME = json['project_name'] or __name__
 
-@app.route("/pokemon", methods=['GET'])
-def get_pokemon():
-    return {
-        'name': 'Pikachu',
-        'color': 'Yellow',
-        'type': 'Lightning'
-    }
+class DefaultJsonMapper(object):
+    def map(self, endpoint, json): return json
 
-if __name__ == '__main__':
-    app.run(debug=True)
+class FlexApp(FlaskAPI):
+    def __init__(self):
+        super(FlexApp, self).__init__(PROJECT_NAME)
+        self.json_mapper = DefaultJsonMapper()
+
+app = FlexApp()
+
+class BaseApi(object):
+    '''
+    Passthrough routing methods for the PokeAPI endpoints.
+    Accepts an optional json_mapper object that can transform
+    json using the map method. See DefaultJsonMapper for the 
+    expected signature for map.
+    '''
+    def __init__(self, json_mapper=None):
+        self.app = app
+        if json_mapper is not None:
+            app.json_mapper = json_mapper
+    
+    def run(self, debug):
+        return self.app.run(debug=debug)
+
+    @app.route("/pokemon", methods=['GET'])
+    def pokemon():
+        json = {
+            'name': 'Pikachu',
+            'color': 'Yellow',
+            'type': 'Electric'
+        }
+        return app.json_mapper.map("/pokemon", json)
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from api import BaseApi
+
+api = BaseApi()
+
+if __name__ == '__main__':
+   api.run(debug=True)
