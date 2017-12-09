162b6eb4e3351f9ae2bf3477b90567f7195092d9
mpcarolin <mpcarolin@gmail.com>
2017-12-09 12:04:06 -0700

    JsonMapper is now ResponseMapper and accepts a response object + FIXES

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-from mapping import JsonMapper
+from mapping import ResponseMapper
+from constants import ENDPOINTS
 
-mapper = JsonMapper()
+mapper = ResponseMapper()
 
-@mapper.maps('/pokemon', '/berry')
-def pokemon_mapper(self, json):
-    new_json = json
+@mapper.maps(ENDPOINTS['pokemon-by-name'], '/berry')
+def pokemon_mapper(self, response):
+    new_json = response.json()
     new_json['weight'] = 78
     return new_json
 
